bb1edf2e2683b20a7dc86b3df088d2215d274dda
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-02-19 13:53:29 -0600

    add keybind <leader>dd to display diagnostic for current line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>dd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Toggles local troubleshoot" })
